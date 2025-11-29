04b4701b6fca5ccd096048110373a3dfcafff921
iKhqled9 <xikhaled9@gmail.com>
2025-11-29 17:56:19 +0300

    Create ProductRepository.java

---
+++ src/main/java/com/gamehub/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.gamehub.repository;
+
+import com.gamehub.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
